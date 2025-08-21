3ee51b2264f0d582f52c717612ba9a6b22e718bc
Luiz Henrique Baldissera <134062116+LuizBaldissera@users.noreply.github.com>
2025-08-21 10:40:59 -0300

    Update MemoriaLuiz.c

--- MemoriaLuiz.c
+++ MemoriaLuiz.c
@@ -9,19 +9,20 @@ int main() {
     int resultado[TAMANHO_LISTA];  // atribuicao
     
     
-    multiplicarListas(lista1, lista2, resultado, TAMANHO_LISTA); // chama funcao com parametros (atribuicao)
+    multiplicarListas(lista1, lista2, resultado, TAMANHO_LISTA); // chama funcao com parametros (atribuicao 4)
     
     
     printf("Resultado da multiplicação das listas:\n"); // atribuicao
     for (int i = 0; i < TAMANHO_LISTA; i++) {       // loop (comparacao + atribuicao + operacao)
-        printf("%d ", resultado[i]);    //atribuicao
+        printf("%d ", resultado[i]);    //atribuicao + atribuição
     }
     
-    return 0;
+    return 0; //atribuição
 }
 
 void multiplicarListas(int lista1[], int lista2[], int resultado[], int tamanho) { // parametros (atribuicao)
     for (int i = 0; i < tamanho; i++) {       // loop (comparacao + atribuicao + operacao)
-        resultado[i] = lista1[i] * lista2[i];    //operacao matematica
+        resultado[i] = lista1[i] * lista2[i];    //operacao matematica + atribuição
     }
 }
+
