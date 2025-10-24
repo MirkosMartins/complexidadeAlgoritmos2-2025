910b6b2f9f5c581d7995b35bbb035f93d5b6c372
Emanuel Bissacotti <98053136+Emanuel-Bissacotti@users.noreply.github.com>
2025-10-24 10:29:36 -0300

    Update main.py

--- Desafio-GrupoAcademia/main.py
+++ Desafio-GrupoAcademia/main.py
@@ -1,21 +1,25 @@
 from string import ascii_letters
 import time
 
-palavra = input("Digite a palavra: ")
+# palavra = input("Digite a palavra: ")
+with open('shrek.txt', 'r') as arquivo:
+    palavra = arquivo.read() # Lê todo o conteúdo
+    palavra = palavra.replace('\n\n', '\n')
 
 def conta_consoante_vogais_qtd(palavra):
     start = time.time()
-    consoantes = set(ascii_letters) - set('aeiou')
-    vogais = set('aeiou')
+    consoantes = set(ascii_letters) - set('aeiouâãáàéíìèúó')
+    vogais = set('aeiouâãáàéíìèúó')
     soma_c = 0
     soma_v = 0
     soma = 0
     for c in palavra.lower():
-        if c != " ":
+        if c != " " and c != "\n":
             if c in consoantes:
                 soma_c += 1
             elif c in vogais:
                 soma_v += 1
+        elif c == " " or c == "\n":
             soma += 1
     fim = time.time()
     print(fim-start)
